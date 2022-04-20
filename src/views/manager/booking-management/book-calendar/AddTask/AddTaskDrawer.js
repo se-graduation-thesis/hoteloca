1b688c7f1d9906d3dd4a50c9cf114e6af639a54c
NguyenThanhHoai-SE <hoai.se.iuh@gmail.com>
2022-04-20 20:46:04 +0700

    commit 200420222045

--- src/views/manager/booking-management/book-calendar/AddTask/AddTaskDrawer.js
+++ src/views/manager/booking-management/book-calendar/AddTask/AddTaskDrawer.js
@@ -1,14 +1,17 @@
 import CloseIcon from '@mui/icons-material/Close';
-import { Box, Button, Drawer, IconButton } from "@mui/material";
+import { Box, Button, Drawer, IconButton, Step, StepButton, StepLabel, Stepper, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import React from "react";
-import AddTaskForm from "./AddTaskForm";
+// import AddTaskForm from "./AddTaskForm";
+import * as React from 'react';
+import CustomerInfo from './components/customerInfo';
 
 const useStyles = makeStyles((theme) => ({
   drawerWrapper: {
     padding: "40px",
   },
 }));
+
+const steps = ['Thông tin khách hàng', 'Thông tin đặt phòng', 'Thông tin dịch vụ'];
 
 function AddTaskDrawer(props) {
   const classes = useStyles();
@@ -25,16 +28,128 @@ function AddTaskDrawer(props) {
     props.handleStateForm(open);
   };
 
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [completed, setCompleted] = React.useState({});
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+        // find the first step that has been completed
+        steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
   const list = (anchor) => (
-    <Box
-      sx={{ width: 700, padding: '20px 30px 30px 30px' }}
-      role="presentation"
-    >
-      {/* <IconButton style={{ float: 'right', marginBottom: '20px', cursor: 'pointer' }}
-        onClick={toggleDrawer(false)}>
-        <CloseIcon />
-      </IconButton> */}
-      <AddTaskForm handleStateForm={props.handleStateForm} dateChoice={props.dateChoice} />
+    // <Box
+    //   sx={{ width: 700, padding: '20px 30px 30px 30px' }}
+    //   role="presentation"
+    // >
+    //   {/* <IconButton style={{ float: 'right', marginBottom: '20px', cursor: 'pointer' }}
+    //     onClick={toggleDrawer(false)}>
+    //     <CloseIcon />
+    //   </IconButton> */}
+    //   <AddTaskForm handleStateForm={props.handleStateForm} dateChoice={props.dateChoice} />
+    // </Box>
+    <Box sx={{ width: 700, padding: '20px 30px 30px 30px' }}>
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {allStepsCompleted() ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you're finished
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Đặt lại</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div>
+              {activeStep === 0 ?
+                <CustomerInfo /> :
+                activeStep === 1 ?
+                  <></> :
+                  <></>
+              }
+            </div>
+            <div style={{ position: 'fixed', bottom: 100 }}>
+              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1 }}
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: '1 1 auto', ml: 40 }} />
+                <Button onClick={handleNext} sx={{ mr: 1 }}>
+                  Next
+                </Button>
+                {activeStep !== steps.length &&
+                  (completed[activeStep] ? (
+                    <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                      Bước {activeStep + 1} đã hoàn thành
+                    </Typography>
+                  ) : (
+                    <Button onClick={handleComplete}>
+                      {completedSteps() === totalSteps() - 1
+                        ? 'Kết Thúc'
+                        : 'Hoàn Thành Bước'}
+                    </Button>
+                  ))}
+              </Box>
+            </div>
+          </React.Fragment>
+        )}
+      </div>
     </Box>
   );
 
---
+++ src/views/manager/booking-management/book-calendar/AddTask/components/customerInfo.js
@@ -0,0 +1,198 @@
+import { FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { address } from 'assets/address';
+import { useState } from "react";
+
+export default function CustomerInfo() {
+
+    const [customer, setCustomer] = useState({
+        ho: '',
+        ten: '',
+        cmnd: '',
+        diaChi: '',
+        dienThoai: '',
+        email: '',
+        quocTich: 'Việt Nam',
+        soHoChieu: '',
+        trangThai: 1,
+        password: ''
+    })
+
+    const [district, setDistrict] = useState([])
+    const [wards, setWards] = useState([])
+    const [tinh, setTinh] = useState('');
+    const [huyen, setHuyen] = useState('');
+    const [xa, setXa] = useState('');
+
+    const getDistrict = (a) => {
+        setDistrict(a.districts)
+        setWards([])
+    }
+
+    const getWards = (a) => {
+        setWards(a.wards)
+    }
+
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sx={{ marginTop: 5 }}>
+                    <Typography sx={{ fontWeight: 'bold', fontSize: 18 }}>Thông tin khách hàng</Typography>
+                </Grid>
+
+                {/* HỌ - Tên */}
+                <Grid item xs={6}>
+                    <TextField
+                        value={customer.ho}
+                        id="outlined-basic"
+                        label="Họ"
+                        variant="outlined"
+                        fullWidth
+
+                        onChange={(e) => setCustomer({ ...customer, ho: e.target.value })}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        value={customer.ten}
+                        id="outlined-basic"
+                        label="Tên"
+                        variant="outlined"
+                        fullWidth
+
+                        onChange={(e) => setCustomer({ ...customer, ten: e.target.value })}
+                    />
+                </Grid>
+
+                {/* CMND - Quốc tịch */}
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <TextField
+                        value={customer.cmnd}
+                        autoFocus
+                        id="outlined-basic"
+                        label="Chứng Minh Nhân Dân"
+                        variant="outlined"
+                        fullWidth
+
+                        onChange={(e) => setCustomer({ ...customer, cmnd: e.target.value })}
+                    />
+                </Grid>
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <TextField
+                        value={customer.quocTich}
+                        id="outlined-basic"
+                        label="Quốc Tịch"
+                        variant="outlined"
+                        fullWidth
+
+                        onChange={(e) => setCustomer({ ...customer, quocTich: e.target.value })}
+                    />
+                </Grid>
+                {
+                    customer.quocTich !== "Việt Nam" ?
+                        <Grid item xs={12} sx={{ marginTop: 2 }}>
+                            <TextField
+                                value={customer.soHoChieu}
+                                id="outlined-basic"
+                                label="Số Hộ Chiếu"
+                                variant="outlined"
+                                fullWidth
+
+                                onChange={(e) => setCustomer({ ...customer, soHoChieu: e.target.value })}
+                            />
+                        </Grid> : <></>
+                }
+
+                {/* CMND - Quốc tịch */}
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <TextField
+                        value={customer.dienThoai}
+                        id="outlined-basic"
+                        label="Điện Thoại"
+                        variant="outlined"
+                        fullWidth
+
+                        onChange={(e) => setCustomer({ ...customer, dienThoai: e.target.value })}
+                    />
+                </Grid>
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <TextField
+                        value={customer.email}
+                        id="outlined-basic"
+                        label="Email"
+                        variant="outlined"
+                        fullWidth
+
+                        onChange={(e) => setCustomer({ ...customer, email: e.target.value })}
+                    />
+                </Grid>
+
+                {/* Địa Chỉ */}
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Tỉnh / Thành phố</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Tỉnh / Thành phố"
+                            value={tinh}
+                            onChange={(e) => setTinh(e.target.value)}
+                        >
+                            {
+                                address.map((a) => (
+                                    <MenuItem key={a.id} value={a.name} onClick={() => getDistrict(a)}>{a.name}</MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Quận / Huyện</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Quận / Huyện"
+                            value={huyen}
+                            onChange={(e) => setHuyen(e.target.value)}
+                        >
+                            {
+                                district.map((a) => (
+                                    <MenuItem key={a.id} value={a.name} onClick={() => getWards(a)}>{a.name} </MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Xã / Phường</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Xã / Phường"
+                            value={xa}
+                            onChange={(e) => setXa(e.target.value)}
+                        >
+                            {
+                                wards.map((a) => (
+                                    <MenuItem key={a.id} value={a.name} > {a.prefix + " " + a.name} </MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={6} sx={{ marginTop: 2 }}>
+                    <TextField
+                        value={customer.diaChi}
+                        id="outlined-basic"
+                        label="Địa chỉ"
+                        variant="outlined"
+                        fullWidth
+
+                        onChange={(e) => setCustomer({ ...customer, diaChi: e.target.value })}
+                    />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
