eaa8d9f2791b3b7628a31047f839c087ebe30b08
Tuan Khang <khangletuan098@gmail.com>
2022-06-06 15:21:15 +0700

    fix

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseApi = 'http://localhost:5000/api/';
+const baseApi = 'https://spring-api-hoteloca.herokuapp.com/api/';
 // const SOCKET_URL = "ws://localhost:3030/";
 
 export default {
